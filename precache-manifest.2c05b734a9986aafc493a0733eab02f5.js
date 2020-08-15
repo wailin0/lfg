e663b723f4437e02f458f734105ef2594c6b35a3
WaiLin <leon.wai@live.com>
2020-08-16 02:07:29 +0630

    Updates

---
+++ precache-manifest.2c05b734a9986aafc493a0733eab02f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bdc8f73687bf4fce5e9fe6d2f36db5a",
+    "url": "/lfg/index.html"
+  },
+  {
+    "revision": "ee32e9e0f0b555053138",
+    "url": "/lfg/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7da37cd80a153583727d",
+    "url": "/lfg/static/css/main.4e69e6a0.chunk.css"
+  },
+  {
+    "revision": "ee32e9e0f0b555053138",
+    "url": "/lfg/static/js/2.a56f8c37.chunk.js"
+  },
+  {
+    "revision": "3212e0c2786bba3b0defa9e5fe1e059e",
+    "url": "/lfg/static/js/2.a56f8c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7da37cd80a153583727d",
+    "url": "/lfg/static/js/main.de877ebc.chunk.js"
+  },
+  {
+    "revision": "dc9095e28b22a2870d87",
+    "url": "/lfg/static/js/runtime-main.04c4ad8b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lfg/precache-manifest.96b0c99fb850f586d75db0641648bb61.js"
+  "/lfg/precache-manifest.2c05b734a9986aafc493a0733eab02f5.js"
 );
 
 self.addEventListener('message', (event) => {
