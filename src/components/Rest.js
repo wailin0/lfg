cbbb1d344f219536944e3eeb8641e21ecc226c65
wailin0 <61951765+wailin0@users.noreply.github.com>
2020-11-06 22:55:05 +0630

    changed backend server account

--- src/components/Rest.js
+++ src/components/Rest.js
@@ -1,3 +1,3 @@
 let Rest;
 // export default Rest = 'http://localhost:8080/api'
- export default Rest = 'https://lfg-spring.herokuapp.com/api'
+ export default Rest = 'https://lfgspring.herokuapp.com/api'
